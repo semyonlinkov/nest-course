96d1f29ed469d6df86eb3df2fe2dad9e5ed33b8e
Semyon Linkov <semyonlinkov@gmail.com>
2025-04-13 02:17:46 +0400

    init_prisma_2

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    constructor(private prisma: PrismaService) { }
+
+    getHello(): any {
+        return this.prisma.user.create({
+            data: {
+                name: 'pidor',
+                email: 'pidor@gay.ru'
+            }
+        });
+    }
 }
